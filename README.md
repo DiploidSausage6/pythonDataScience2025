8a622939847ecf2095b663c5698ea7b49bc8815f
David Farkas <davidfarkas@Steves-MacBook-Pro.local>
2025-09-28 21:18:56 -0400

    my first commit of my first python code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pythonDataScience2025
 My Python class in 2025
+
+![](./assets/pythoncool.png)
---
+++ ReverseString.py
@@ -0,0 +1,23 @@
+# Write a function to reverse a string
+
+# Ask the user to enter something
+string = input("Enter a string in quotes please: ")
+
+# To reverse it I think I need to break it up into chunks first that I can look at backwards.
+# A function to reverse the string:
+def reverseIt(name):
+    # To reverse it I need to break it into smaller parts first so I'll make it a list
+    nameCharactersList = list(name)
+    print(nameCharactersList)
+    # now I can reverse that list like reading it backwards
+    # I found that Python has a built in reversed function to reverse lists for you
+    nameCharsReversed = list(reversed(nameCharactersList))
+    print(nameCharsReversed)
+    # I don't want it as a list so need to combine back into a string
+    reversedName = "".join(nameCharsReversed)
+    return reversedName # send back the reversed one
+
+reversedString = reverseIt(string) # here I call the function
+
+print("And here it is combined back into a string")
+print(reversedString)
