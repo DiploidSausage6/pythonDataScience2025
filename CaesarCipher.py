22e5fd9a1611ccf41155eb28ff0596199a81ce6f
david <dlfarkas12@gmail.com>
2025-10-25 18:17:23 -0400

    Update and rename CeasarCipher.py to CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -4,4 +4,4 @@ alphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"] # make a list
 
 def Encryptor (beforeMessage):
   beforeMessage = input("enter message you wish to be encrypted:")
-  print (beforeMessage)
+  
