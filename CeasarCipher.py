6a1839318e5562db659ea892c51cd8259b75b662
david <dlfarkas12@gmail.com>
2025-10-25 17:54:54 -0400

    Modify Encryptor to prompt for message input
    Update Encryptor function to take user input for message.

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -3,4 +3,5 @@
 alphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"] # make a list of the alphabet to use index numbers later
 
 def Encryptor (beforeMessage)
-  print(bob)
+  beforeMessage = input("enter message you wish to be encrypted:")
+  print beforeMessage
