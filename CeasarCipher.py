c9f30de44bf552df2313366cb5fce71e2e026148
david <dlfarkas12@gmail.com>
2025-10-25 17:40:23 -0400

    Initialize alphabet and add comment in CeasarCipher.py
    Added a comment and initialized the alphabet list for encryption.

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -1,1 +1,6 @@
+# this is a program to encrypt a string via the Ceasar Cipher
 
+alphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"] # make a list of the alphabet to use index numbers later
+
+def Encryptor (beforeMessage)
+  print(bob)
