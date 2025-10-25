50457070a8a2ece16e8029175f0f0dcc279aa3c9
david <dlfarkas12@gmail.com>
2025-10-25 18:06:16 -0400

    Correct Encryptor function definition and input
    Fix function definition and input handling in Encryptor.

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -2,6 +2,6 @@
 
 alphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"] # make a list of the alphabet to use index numbers later
 
-def Encryptor (beforeMessage)
+def Encryptor (beforeMessage):
   beforeMessage = input("enter message you wish to be encrypted:")
   print beforeMessage
